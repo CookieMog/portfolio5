7a622102f435f0faa4658de7e8f5893d3e94b055
CookieMog <31330690+CookieMog@users.noreply.github.com>
2023-05-29 11:55:18 -0400

    Change db pour hosting

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,13 +1,13 @@
 <header>
     <div id="banner">
         <div class="banner-image-wrapper">
-            <img src="https://via.placeholder.com/630x288" alt="imageBanniere1" class="banner-image" id="banner1">
+            <img src="{{ Storage::url('images/V.jpg') }}" alt="imageBanniere1" class="banner-image" id="banner1">
         </div>
         <div class="banner-image-wrapper">
-            <img src="https://via.placeholder.com/630x288" alt="imageBanniere2" class="banner-image" id="banner2">
+            <img src="{{ Storage::url('images/S.jpg') }}" alt="imageBanniere2" class="banner-image" id="banner2">
         </div>
         <div class="banner-image-wrapper">
-            <img src="https://via.placeholder.com/630x288" alt="imageBanniere3" class="banner-image" id="banner3">
+            <img src="{{ Storage::url('images/C.jpg') }}" alt="imageBanniere3" class="banner-image" id="banner3">
         </div>
     </div>
 </header>
